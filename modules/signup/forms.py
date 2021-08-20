b8c7d438dc63d55911b1a134035d016d345eba21
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-08-20 23:47:29 +0530

    Use inheritance

--- modules/signup/forms.py
+++ modules/signup/forms.py
@@ -18,16 +18,7 @@ class SignupForm(forms.Form):
     Account_type = forms.ChoiceField(choices=[('Human', 'Human'), ('Bot', 'Bot')])
 
 
-class EditProfileForm(forms.Form):
-    member1 = forms.CharField(label='Team Player 1', max_length=100)
-    member2 = forms.CharField(label='Team Player 2', max_length=100)
-    member3 = forms.CharField(label='Team Player 3', max_length=100)
-    member4 = forms.CharField(label='Team Player 4', max_length=100)
-    member5 = forms.CharField(label='Team Player 5', max_length=100)
-    member6 = forms.CharField(label='Team Player 6', max_length=100)
-    member7 = forms.CharField(label='Team Player 7', max_length=100)
-    member8 = forms.CharField(label='Team Player 8', max_length=100)
-    member9 = forms.CharField(label='Team Player 9', max_length=100)
-    member10 = forms.CharField(label='Team Player 10', max_length=100)
-    member11 = forms.CharField(label='Team Player 11', max_length=100)
-    password = forms.CharField(widget=PasswordInput) 
+class EditprofileForm(SignupForm):
+    team_name = forms.CharField(label='Team name', widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    password = forms.CharField(widget=forms.PasswordInput(render_value=True))
+    Account_type = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
