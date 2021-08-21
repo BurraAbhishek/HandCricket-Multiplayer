1bfc8b1be8bb3bc1e1a90898f6c7f79b062d63a3
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-08-21 23:54:24 +0530

    Render license

---
+++ modules/docs/license.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from modules.css import choose
+from modules.css.iconset import settings_icon
+
+def open(request):
+    signin = "Sign In"
+    signin_action = "/login/"
+    if 'team' in request.session:
+        signin = request.session['team']
+        signin_action = "/profile/"
+    return render(
+        request, 
+        'docs/license.html', 
+        {
+            "appbar": 'templates/appbar.html',
+            "settingsicon": settings_icon,
+            "signin": signin,
+            "signin_action": signin_action,
+            "stylesheet": choose.getTheme()
+        }
+    )
